1dfaa54e7ecc07098970748210a4aae3eed7bbfc
abaker93 <anna.baker.design@gmail.com>
2020-10-03 20:48:02 -0400

    book constructor

---
+++ main.js
@@ -0,0 +1,21 @@
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    let readText = '';
+    
+    if(this.read === true) {
+        readText = 'already read'
+    } else {
+        readText = 'not read yet'
+    }
+
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${readText}`
+    }
+}
+
+const theHobbit = new book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+
+console.log(theHobbit.info());
