1bc8af3dad5b7397e5e7b38d78c2ebe148f84183
abaker93 <anna.baker.design@gmail.com>
2020-10-04 16:18:41 -0400

    add books to list

--- main.js
+++ main.js
@@ -1,18 +1,6 @@
-let library = [
-	{
-		title: 'The Hobbit',
-		author: 'J.R.R. Tolkien',
-		pages: 295,
-		read: false
-	},
-	{
-		title: 'Lord of the Rings',
-		author: 'Tolkien',
-		pages: 100,
-		read: true
-	}
-];
-const libraryList = document.getElementById('library');
+let library = [];
+const libraryList = document.getElementById('library-list');
+const formModal = document.getElementById('form-modal');
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -21,19 +9,23 @@ function Book(title, author, pages, read) {
 	this.read = read;
 }
 
-Book.prototype.readText = function() {
-	if(this.read) {
-		return 'read';
+function addBookToLibrary() {
+	let a = document.getElementById('form-title').value;
+	let b = document.getElementById('form-author').value;
+	let c = document.getElementById('form-pages').value;
+	let d = document.getElementById('form-read').checked;
+
+	if (a !== '' && b !== '' && c !== '' ) {
+		library.push(new Book(a, b, c, d));
+		displayBooks();
+		clearForm();
 	} else {
-		return 'not read';
+		alert('Form not filled out')
 	}
 }
 
-function addBookToLibrary() {
-	
-}
-
 function displayBooks() {
+	libraryList.innerHTML = '';
 	for(let i=0; i<library.length; i++) {
 		const libraryItem = document.createElement('div');
 		libraryItem.classList.add('library-row');
@@ -43,9 +35,38 @@ function displayBooks() {
 			<div>${library[i].author}</div>
 			<div>${library[i].pages}</div>
 			<div>${library[i].read}</div>
-			<button>X</button>`;
+			<button class="remove-btn" onclick="removeBook()">X</button>`;
 		libraryList.append(libraryItem);
 	}
 }
 
 displayBooks();
+
+function clearForm() {
+	document.getElementById('form-title').value = '';
+	document.getElementById('form-author').value = '';
+	document.getElementById('form-pages').value = '';
+	document.getElementById('form-read').checked = false;
+}
+
+// function removeBook() {
+// 	const removeBtns = document.querySelectorAll('.remove-btn');
+// 	const libraryRows = document.querySelectorAll('.library-row');
+// 	console.log(removeBtns);
+// 	console.log(libraryRows.length);
+
+// 	removeBtns.forEach(btn => {
+// 		btn.addEventListener('click', e => {
+// 			let bookID = e.target.parentElement.getAttribute('data-index');
+// 			console.log(bookID);
+// 			for (let i=0; i<libraryRows.length; i++) {
+// 				console.log(libraryRows.dataset);
+// 			}
+// 		})
+// 	})
+// }
+
+function showModal() {
+	formModal.classList.remove('hide');
+	formModal.classList.add('show');
+}
